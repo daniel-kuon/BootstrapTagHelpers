60bc2675c667bf968cb1bbdd51db8bf7bb673010
Daniel Kuon <dakuon@gmx.net>
2015-07-13 20:49:33 +0200

    closes #9

---
+++ BootstrapTagHelpers/src/BootstrapTagHelpers/ListTagHelper.cs
@@ -0,0 +1,47 @@
+﻿namespace BootstrapTagHelpers {
+    using System.Collections.Generic;
+
+    using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+    [TargetElement("ul",Attributes = AttributePrefix + "unstyled")]
+    [TargetElement("ul",Attributes = AttributePrefix + "inline")]
+    [TargetElement("ol",Attributes = AttributePrefix + "unstyled")]
+    [TargetElement("ol",Attributes = AttributePrefix + "inline")]
+    public class ListTagHelper:BootstrapTagHelper {
+
+        [HtmlAttributeName(AttributePrefix+"unstyled")]
+        public bool Unstyled { get; set; }
+
+        [HtmlAttributeName(AttributePrefix+"inline")]
+        public bool Inline { get; set; }
+        
+        protected override void BootstrapProcess(TagHelperContext context, TagHelperOutput output) {
+            if (context.IsSet(()=>Unstyled))
+                output.AddCssClass("list-unstyled");
+            if (context.IsSet(()=>Inline))
+                output.AddCssClass("list-inline");
+        }
+    }
+
+    public class TableTagHelper : BootstrapTagHelper {
+
+        [HtmlAttributeName(AttributePrefix + "stripped")]
+        public bool Stripped { get; set; }
+
+        [HtmlAttributeName(AttributePrefix + "condensed")]
+        public bool Condensed { get; set; }
+
+        [HtmlAttributeName(AttributePrefix + "bordered")]
+        public bool Bordered { get; set; }
+
+        [HtmlAttributeName(AttributePrefix + "responsive")]
+        public bool Responsive { get; set; }
+
+        [HtmlAttributeName(AttributePrefix + "hover")]
+        public bool Hover { get; set; }
+
+        protected override void BootstrapProcess(TagHelperContext context, TagHelperOutput output) {
+            var classes = new List<string>() { "table"};
+        }
+    }
+}
