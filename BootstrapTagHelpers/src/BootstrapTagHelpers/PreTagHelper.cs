e084e6520e031d09fc9fc2a01b2bf20a0e4ec55d
Daniel Kuon <dakuon@gmx.net>
2015-07-13 20:56:39 +0200

    closes #15

---
+++ BootstrapTagHelpers/src/BootstrapTagHelpers/PreTagHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace BootstrapTagHelpers {
+    using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+    [TargetElement("pre",Attributes = AttributePrefix+"scrollable")]
+    public class PreTagHelper :BootstrapTagHelper{
+
+        [HtmlAttributeName(AttributePrefix+"scrollable")]
+        public bool Scrollable { get; set; }
+
+        
+        protected override void BootstrapProcess(TagHelperContext context, TagHelperOutput output) {
+            if (context.IsSet(()=>Scrollable))
+                output.AddCssClass("pre-scrollable");
+        }
+    }
+}
