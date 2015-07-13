889be1d90703e2867b7f9fbe6c13f4160f80e902
Daniel Kuon <dakuon@gmx.net>
2015-07-13 20:46:20 +0200

    closes #10

---
+++ BootstrapTagHelpers/src/BootstrapTagHelpers/AbbrTagHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace BootstrapTagHelpers {
+    using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+    [TargetElement("abbr",Attributes = AttributePrefix + "initialism")]
+    public class AbbrTagHelper:BootstrapTagHelper {
+
+        [HtmlAttributeName(AttributePrefix + "initialism")]
+        public bool Initialism { get; set; }
+
+
+        protected override void BootstrapProcess(TagHelperContext context, TagHelperOutput output) {
+            if (context.IsSet(()=>Initialism))
+                output.AddCssClass("initialism");
+        }
+    }
+
+}
