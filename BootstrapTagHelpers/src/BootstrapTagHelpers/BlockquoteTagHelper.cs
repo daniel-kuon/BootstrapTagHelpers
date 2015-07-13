7b26f53be2e3748a0546690952d50ee66c82cbf6
Daniel Kuon <dakuon@gmx.net>
2015-07-13 20:49:55 +0200

    closes #6

---
+++ BootstrapTagHelpers/src/BootstrapTagHelpers/BlockquoteTagHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace BootstrapTagHelpers {
+    using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+    [TargetElement("blockquote",Attributes = AttributePrefix + "reverse")]
+    public class BlockquoteTagHelper:BootstrapTagHelper
+    {
+        [HtmlAttributeName(AttributePrefix+"reverse")]
+        public bool Reverse { get; set; }
+
+        protected override void BootstrapProcess(TagHelperContext context, TagHelperOutput output) {
+            if (context.IsSet(()=>Reverse))
+                output.AddCssClass("blockquote-reverse");
+        }
+    }
+
+}
