386d6b8a9401d02f1b1872fa48d337e7775a0364
Daniel Kuon <dakuon@gmx.net>
2015-07-13 20:46:54 +0200

    closes #10

---
+++ BootstrapTagHelpers/src/BootstrapTagHelpers/DlTagHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace BootstrapTagHelpers {
+    using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+    [TargetElement("dl",Attributes = AttributePrefix +"horizontal")]
+    public class DlTagHelper:BootstrapTagHelper {
+        [HtmlAttributeName(AttributePrefix+"horizontal")]
+        public bool Horizontal { get; set; }
+
+        
+        protected override void BootstrapProcess(TagHelperContext context, TagHelperOutput output) {
+            if (context.IsSet(() => Horizontal))
+                output.AddCssClass("dl-horizontal");
+        }
+    }
+
+}
