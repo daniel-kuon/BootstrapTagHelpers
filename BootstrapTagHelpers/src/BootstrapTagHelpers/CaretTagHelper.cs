308ce0d6e01540a8a09460e5d4fef5fd72aef076
Daniel Kuon <daniel.kuon@outlook.com>
2015-07-26 22:52:30 +0200

    closes #19

---
+++ BootstrapTagHelpers/src/BootstrapTagHelpers/CaretTagHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace BootstrapTagHelpers {
+    using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+    public class CaretTagHelper : BootstrapTagHelper {
+        protected override void BootstrapProcess(TagHelperContext context, TagHelperOutput output) {
+            output.TagName = "span";
+            output.AddCssClass("caret");
+            output.SelfClosing = false;
+        }
+    }
+
+}
