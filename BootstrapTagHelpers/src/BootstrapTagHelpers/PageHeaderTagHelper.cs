1c68657e8568477d7d90ce4fcb1cc6648e00b007
Daniel-Kuon <daniel.kuon@outlook.com>
2015-12-06 19:31:38 +0100

    closes #37

---
+++ BootstrapTagHelpers/src/BootstrapTagHelpers/PageHeaderTagHelper.cs
@@ -0,0 +1,11 @@
+namespace BootstrapTagHelpers {
+    using Microsoft.AspNet.Razor.TagHelpers;
+
+    [OutputElementHint("div")]
+    public class PageHeaderTagHelper : BootstrapTagHelper {
+        protected override void BootstrapProcess(TagHelperContext context, TagHelperOutput output) {
+            output.TagName = "div";
+            output.AddCssClass("page-header");
+        }
+    }
+}
