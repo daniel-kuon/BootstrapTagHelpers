2f8118aa423bbc4b7d3553462c264f34782725fe
Daniel-Kuon <daniel.kuon@outlook.com>
2015-12-13 23:09:56 +0100

    closes #25

--- BootstrapTagHelpers/src/BootstrapTagHelpers/TagHelperContextItemsExtensionMethods.cs
+++ BootstrapTagHelpers/src/BootstrapTagHelpers/TagHelperContextItemsExtensionMethods.cs
@@ -20,5 +20,45 @@ namespace BootstrapTagHelpers {
                 return null;
             return context.Items[ProgressContext] as ProgressTagHelper;
         }
+
+        private const string ButtonToolbarContext = "ButtonToolbarContext";
+
+        public static bool HasButtonToolbarContext(this TagHelperContext context) {
+            return context.Items.ContainsKey(ButtonToolbarContext) && context.Items[ButtonToolbarContext] is ButtonToolbarTagHelper;
+        }
+
+        public static void SetButtonToolbarContext(this TagHelperContext context, ButtonToolbarTagHelper tagHelper) {
+            if (context.Items.ContainsKey(ButtonToolbarContext))
+                context.Items[ButtonToolbarContext] = tagHelper;
+            else
+                context.Items.Add(ButtonToolbarContext, tagHelper);
+        }
+
+        public static ButtonToolbarTagHelper GetButtonToolbarContext(this TagHelperContext context) {
+            if (!context.Items.ContainsKey(ButtonToolbarContext))
+                return null;
+            return context.Items[ButtonToolbarContext] as ButtonToolbarTagHelper;
+        }
+
+        private const string ButtonGroupContext = "ButtonGroupContext";
+
+        public static bool HasButtonGroupContext(this TagHelperContext context) {
+            return context.Items.ContainsKey(ButtonGroupContext) && context.Items[ButtonGroupContext] is ButtonGroupTagHelper;
+        }
+
+        public static void SetButtonGroupContext(this TagHelperContext context, ButtonGroupTagHelper tagHelper) {
+            if (context.Items.ContainsKey(ButtonGroupContext))
+                context.Items[ButtonGroupContext] = tagHelper;
+            else
+                context.Items.Add(ButtonGroupContext, tagHelper);
+        }
+
+        public static ButtonGroupTagHelper GetButtonGroupContext(this TagHelperContext context) {
+            if (!context.Items.ContainsKey(ButtonGroupContext))
+                return null;
+            return context.Items[ButtonGroupContext] as ButtonGroupTagHelper;
+        }
+
+
     }
 }
