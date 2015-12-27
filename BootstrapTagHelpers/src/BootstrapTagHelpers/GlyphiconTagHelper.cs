ac07feaeb0f6669f44183823735fcedfcb5197a4
Daniel-Kuon <daniel.kuon@outlook.com>
2015-12-27 23:45:04 +0100

    closes #21

---
+++ BootstrapTagHelpers/src/BootstrapTagHelpers/GlyphiconTagHelper.cs
@@ -0,0 +1,15 @@
+namespace BootstrapTagHelpers {
+    using Microsoft.AspNet.Razor.TagHelpers;
+
+    [OutputElementHint("span")]
+    [HtmlTargetElement("glyphicon", Attributes = "icon")]
+    public class GlyphiconTagHelper : BootstrapTagHelper {
+        public Glyphicons Icon { get; set; }
+
+        protected override void BootstrapProcess(TagHelperContext context, TagHelperOutput output) {
+            output.TagName = "span";
+            output.AddCssClass("glyphicon");
+            output.AddCssClass(Icon.GetDescription());
+        }
+    }
+}
