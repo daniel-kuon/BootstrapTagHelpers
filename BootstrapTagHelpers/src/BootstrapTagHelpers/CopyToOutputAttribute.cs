6e0f95441bb7a9fef6476c083f9404a71005ca00
Daniel-Kuon <daniel.kuon@outlook.com>
2016-02-07 15:35:07 +0100

    Added abilities to modify name of html attribute to CopyToOutputAttribute

--- BootstrapTagHelpers/src/BootstrapTagHelpers/CopyToOutputAttribute.cs
+++ BootstrapTagHelpers/src/BootstrapTagHelpers/CopyToOutputAttribute.cs
@@ -1,6 +1,7 @@
 namespace BootstrapTagHelpers {
     using System;
     using System.Linq;
+    using System.Reflection;
 
     using BootstrapTagHelpers.Extensions;
 
@@ -12,6 +13,27 @@ namespace BootstrapTagHelpers {
     [AttributeUsage(AttributeTargets.Property)]
     public class CopyToOutputAttribute : Attribute {
 
+        public string Prefix { get; set; }
+
+        public string Suffix { get; set; }
+
+        public string OutputAttributeName { get; set; }
+
+        public CopyToOutputAttribute() {
+        }
+
+        public CopyToOutputAttribute(string outputAttributeName) : this(outputAttributeName, null) {
+        }
+
+        public CopyToOutputAttribute(string outputAttributeName, string prefix) : this(outputAttributeName, prefix, null) {
+        }
+
+        public CopyToOutputAttribute(string outputAttributeName, string prefix, string suffix) {
+            Prefix = prefix;
+            Suffix = suffix;
+            OutputAttributeName = outputAttributeName;
+        }
+
         public static void CopyPropertiesToOutput(object target, TagHelperOutput output) {
             if (target == null)
                 throw new ArgumentNullException(nameof(target));
@@ -19,8 +41,9 @@ namespace BootstrapTagHelpers {
                 throw new ArgumentNullException(nameof(output));
             foreach (var propertyInfo in target.GetType().GetProperties().Where(pI => pI.HasCustomAttribute<CopyToOutputAttribute>())) {
                 var value = propertyInfo.GetValue(target);
+                var attr = propertyInfo.GetCustomAttribute<CopyToOutputAttribute>();
                 if (value != null)
-                    output.Attributes.Add(propertyInfo.GetHtmlAttributeName(), value);
+                    output.Attributes.Add(attr.Prefix + (attr.OutputAttributeName??propertyInfo.GetHtmlAttributeName()) + attr.Suffix, value);
             }
         }
     }
