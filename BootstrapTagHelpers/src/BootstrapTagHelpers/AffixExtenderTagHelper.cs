7c1f5b09bc6dd6b76c9a6c4995df0731debdc00a
Daniel-Kuon <daniel.kuon@outlook.com>
2016-02-07 16:32:09 +0100

    Affix closes #61

---
+++ BootstrapTagHelpers/src/BootstrapTagHelpers/AffixExtenderTagHelper.cs
@@ -0,0 +1,44 @@
+﻿using BootstrapTagHelpers.Extensions;
+using Microsoft.AspNet.Razor.TagHelpers;
+
+namespace BootstrapTagHelpers
+{
+    [HtmlTargetElement("*", Attributes = AffixAttributeName)]
+    public class AffixExtenderTagHelper : BootstrapTagHelper
+    {
+        public const string AffixAttributeName = AttributePrefix + "affix";
+
+        [HtmlAttributeName(AffixAttributeName)]
+        [HtmlAttributeMinimizable]
+        [HtmlAttributeNotBound]
+        public bool IsAffix { get; set; }
+
+        [HtmlAttributeName(AttributePrefix + "affix-offset-bottom")]
+        [CopyToOutput("data-offset-bottom")]
+        public int? OffsetBottom { get; set; }
+
+        [HtmlAttributeName(AttributePrefix + "affix-offset-top")]
+        [CopyToOutput("data-offset-bottom")]
+        public int? OffsetTop { get; set; }
+
+        [HtmlAttributeName(AttributePrefix + "affix-offset")]
+        [CopyToOutput("data-offset-bottom")]
+        [CopyToOutput("data-offset-top")]
+        public int? Offset { get; set; }
+
+        [HtmlAttributeName(AttributePrefix + "affix-target")]
+        [CopyToOutput("data-target")]
+        public string Target { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (IsAffix)
+                base.Process(context, output);
+        }
+
+        protected override void BootstrapProcess(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.AddDataAttribute("spy", "affix");
+        }
+    }
+}
---
+++ BootstrapTagHelpers/src/BootstrapTagHelpers/AffixTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using BootstrapTagHelpers.Extensions;
+using Microsoft.AspNet.Razor.TagHelpers;
+
+namespace BootstrapTagHelpers
+{
+    [OutputElementHint("div")]
+    public class AffixTagHelper : BootstrapTagHelper
+    {
+        [CopyToOutput(Prefix = "data-")]
+        public int? OffsetBottom { get; set; }
+
+
+        [CopyToOutput(Prefix = "data-")]
+        public int? OffsetTop { get; set; }
+
+        [CopyToOutput("data-offset-bottom")]
+        [CopyToOutput("data-offset-top")]
+        public int? Offset { get; set; }
+
+        [CopyToOutput(Prefix = "data-")]
+        public string Target { get; set; }
+
+        protected override void BootstrapProcess(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.AddDataAttribute("spy", "affix");
+        }
+    }
+}
