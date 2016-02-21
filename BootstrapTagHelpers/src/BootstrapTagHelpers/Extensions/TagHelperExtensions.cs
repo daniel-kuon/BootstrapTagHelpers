565b499dd1a72694cd2f332e767b07ba5212276a
Daniel-Kuon <daniel.kuon@outlook.com>
2016-02-21 19:13:25 +0100

    TagHelperExtensions Added overloads with TagHelperContext parameter

--- BootstrapTagHelpers/src/BootstrapTagHelpers/Extensions/TagHelperExtensions.cs
+++ BootstrapTagHelpers/src/BootstrapTagHelpers/Extensions/TagHelperExtensions.cs
@@ -17,13 +17,29 @@
         public static TagHelperContent ToTagHelperContent(this TagHelper tagHelper, TagHelperContent content) {
             return ToTagHelperContent(tagHelper, tagHelper.GetTagName(), content);
         }
-       
+
         public static TagHelperContent ToTagHelperContent(this TagHelper tagHelper, string tagName) {
-            return ToTagHelperContent(tagHelper, tagName, null);
+            return ToTagHelperContent(tagHelper, tagName, (TagHelperContent)null);
         }
 
         public static TagHelperContent ToTagHelperContent(this TagHelper tagHelper, string tagName, TagHelperContent content) {
             var context = new TagHelperContext(new List<IReadOnlyTagHelperAttribute>(), new Dictionary<object, object>(), Guid.NewGuid().ToString("N"));
+            return ToTagHelperContent(tagHelper, tagName, content, context);
+        }
+
+        public static TagHelperContent ToTagHelperContent(TagHelper tagHelper, TagHelperContext context) {
+            return ToTagHelperContent(tagHelper, tagHelper.GetTagName(), context);
+        }
+
+        public static TagHelperContent ToTagHelperContent(TagHelper tagHelper, string tagName, TagHelperContext context) {
+            return ToTagHelperContent(tagHelper, tagName, null, context);
+        }
+
+        public static TagHelperContent ToTagHelperContent(TagHelper tagHelper, TagHelperContent content, TagHelperContext context) {
+            return ToTagHelperContent(tagHelper, tagHelper.GetTagName(), content, context);
+        }
+
+        public static TagHelperContent ToTagHelperContent(TagHelper tagHelper, string tagName, TagHelperContent content, TagHelperContext context) {
             var output = new TagHelperOutput(tagName, new TagHelperAttributeList(), b => new Task<TagHelperContent>(() => content));
             tagHelper.Process(context, output);
             return output.ToTagHelperContent();
